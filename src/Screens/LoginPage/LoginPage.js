336045828eaaa439d5974bd69800f76b5b110e72
MSS <manansahni567@gmail.com>
2022-02-09 01:45:11 +0530

    Mobile Responsive

--- src/Screens/LoginPage/LoginPage.js
+++ src/Screens/LoginPage/LoginPage.js
@@ -7,10 +7,10 @@ function LoginPage(props) {
     <div className="Login">
       <Navbar />
       <section className="bottom">
-        <div className="Left">
+        <div className="Left col-md-6 col-lg-5 col-xs-12">
           <LoginForm />
         </div>
-        <div className="Right">
+        <div className="Right col-md-6 col-lg-7 col-xs-0">
           <div className="SideImage" />
         </div>
       </section>
